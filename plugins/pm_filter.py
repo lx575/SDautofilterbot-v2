21496247b3332f484c8d34f50837607e6d200638
omgoog <89709117+omgoog@users.noreply.github.com>
2021-12-14 02:00:12 +0530

    Create pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -730,8 +730,7 @@ async def auto_filter(client, msg, spoll=False):
             logger.exception(e)
             await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
     else:
-        await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
-    if spoll:
+        K = await message.reply_text(cap, reply_markup=[InlineKeyboardMarkup(btn)]
         await asyncio.sleep(8)
         await k.delete()
         
