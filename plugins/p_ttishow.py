2281bbd0d4aa31078a7fd255ee0ae785e291df67
omgoog <89709117+omgoog@users.noreply.github.com>
2021-12-16 02:24:13 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -48,13 +48,13 @@ async def save_group(bot, message):
         for u in message.new_chat_members:
             if (temp.MELCOW).get('welcome') is not None:
                 try:
-                    await (temp.MELCOW['welcome']).delete(3)
+                    await (temp.MELCOW['welcome']).delete()
                 except:
                     pass
-            temp.MELCOW['welcome'] = await message.reply(f"<b>Hey , {u.mention}, Welcome to {message.chat.title}</b>")
-                                     await asyncio.sleep(8)
-                                     await temp.MELCOW['welcome'].delete()
-                                      return
+            k = await message.reply(f"<b>Hey , {u.mention}, Welcome to {message.chat.title}</b>")
+                  await asyncio.sleep(8)
+                   await k.delete()
+                      return                   
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
 async def leave_a_chat(bot, message):
