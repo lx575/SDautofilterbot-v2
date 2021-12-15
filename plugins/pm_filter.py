c9bfe5b8cc42ce05280add22589057be0b0e2351
omgoog <89709117+omgoog@users.noreply.github.com>
2021-12-15 20:46:21 +0530

    Create pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -780,7 +780,7 @@ async def advantage_spell_chok(msg):
                     callback_data=f"spolling#{user}#{k}",
                 )
             ] for k, movie in enumerate(movielist)]
-    btn.append([InlineKeyboardButton(text="🗑 𝖢𝗅𝗈𝗌𝖾", callback_data=f'spolling#{user}#close_spellcheck'), InlineKeyboardButton('🔍 𝖦𝗈𝗈𝗀𝗅𝖾', url='https://t.me/https://Google.com')])
+    btn.append([InlineKeyboardButton(text="🗑 𝖢𝗅𝗈𝗌𝖾", callback_data=f'spolling#{user}#close_spellcheck'), InlineKeyboardButton('🔍 𝖦𝗈𝗈𝗀𝗅𝖾', url='https://Google.com')])
     f = await msg.reply("<b>I couldn't find anything related to that\nDid you mean any one of these?</b>", reply_markup=InlineKeyboardMarkup(btn))
     await asyncio.sleep(120)
     await f.delete()
